8cce63d26312d1e7ebce02bc06fe70654790f132
chuck <charlesrymal@gmail.com>
2015-02-12 17:32:19 -0800

    fix pch errors

--- src/Galaxy-Log/log.cpp
+++ src/Galaxy-Log/log.cpp
@@ -1,11 +1,14 @@
 #include <gal/log/log.hpp>
 
-gal::log::base::base(std::ostream& ss): ss_(ss) {
+gal::log::base::base(std::ostream& ss): ss_(ss)
+{
 }
 gal::log::base::base(gal::log::base&& base):
 	ss_(base.ss_)
-{}
-gal::log::base::~base() {
+{
+}
+gal::log::base::~base()
+{
 	ss_ << std::endl;
 }
 
